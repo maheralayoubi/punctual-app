b05eef186e7dc780bfffa628386377d496c9add5
Alzin Mohamad Ghaith <35889862+alzin@users.noreply.github.com>
2021-04-20 23:06:41 +0900

    react beautiful dnd
    Started with an empty columns that has some specs.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,49 @@
-import React from "react";
-import DragableRect from "./DragableRect";
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { v4 as uuidv4 } from 'uuid';
+
+const itemsFromBackend = [
+  { id: uuidv4(), content: "First task" },
+  { id: uuidv4(), content: "Second task" },
+];
+
+const columnsFromBackend = [
+  {
+    [uuidv4()]: {
+      name: "Todo",
+      items: [itemsFromBackend],
+    },
+  },
+];
 
 function App() {
+  const [columns, setColumns] = useState(columnsFromBackend);
   return (
-    <div>
-      <DragableRect />
+    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
+      <DragDropContext onDragEnd={(result) => console.log(result)}>
+        {Object.entries(columns).map(([id, column]) => {
+          return (
+            <Droppable droppableId={id}>
+              {(provided, snapshot) => {
+                return (
+                  <div
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                    style={{
+                      background: snapshot.isDraggingOver
+                        ? "lightblue"
+                        : "lightgrey",
+                      padding: 4,
+                      width: 250,
+                      minHeight: 500,
+                    }}
+                  ></div>
+                );
+              }}
+            </Droppable>
+          );
+        })}
+      </DragDropContext>
     </div>
   );
 }
