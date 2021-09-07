a0f7f73a39717d451cf1ada49cd4e0b928b53ad5
Alzin Mohamad Ghaith <ghaithalzein05@gmail.com>
2021-09-08 08:50:34 +0900

    Add new logic for the tasks with new columns

--- src/components/Tasks/CreateTask.jsx
+++ src/components/Tasks/CreateTask.jsx
@@ -2,100 +2,163 @@ import React, { useState } from "react";
 import Task from "./Task";
 import classes from "./task.module.css";
 import "./task.module.css";
-
+import _ from "lodash";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
-import { v4 as uuidv4 } from "uuid";
+import { v4 } from "uuid";
+
+const action1 = {
+  id: v4(),
+  name: "Study React",
+};
+
+const action2 = {
+  id: v4(),
+  name: "Fly a drone",
+};
 
 export default function CreateTask() {
-  const [task, setTask] = useState({ id: "", content: "" });
-  const [tasks, setTasks] = useState([]);
-
-  const handleChange = (e) => {
-    const {
-      target: { name, value },
-    } = e;
-    setTask((prevTask) => ({
-      ...prevTask,
-      id: uuidv4(),
-      [name]: value,
-    }));
-  };
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState({
+    //creating 3 columns
+    todo: {
+      title: "todo",
+      actions: [action1, action2], //temporary data valuesa
+    },
+    "in-progress": {
+      title: "in-progress",
+      actions: [],
+    },
+    done: {
+      title: "done",
+      actions: [],
+    },
+  });
 
   const addTask = (e) => {
     e.preventDefault();
-    if (task.content) {
-      setTasks((prevTasks) => {
-        return [...prevTasks, task];
-      });
-    }
-    setTask({ id: uuidv4(), content: "" });
+    setTasks((prev) => {
+      return {
+        ...prev,
+        todo: {
+          title: "todo",
+          actions: [{ id: v4(), name: task }, ...prev.todo.actions],
+        },
+      };
+    });
+    setTask("");
   };
 
   const onDragEnd = (result) => {
-    console.log(result);
+    const { destination, draggableId, source } = result;
+    if (!destination) {
+      console.log("No destincation is specified!!");
+      return;
+    }
+
+    if (
+      destination.index === source.index &&
+      destination.droppableId === source.droppableId
+    ) {
+      console.log("Dropped On Myself!!");
+      return;
+    }
+
+    // Creating a copy of item before removing it from state
+    const itemCopy = { ...tasks[source.droppableId].actions[source.index] };
+
+    setTasks((prev) => {
+      prev = { ...prev };
+
+      // if it is not todo column then remove it
+      if (source.droppableId !== "todo") {
+        // Remove from previous items array
+        prev[source.droppableId].actions.splice(source.index, 1);
+      } else {
+        itemCopy.id = v4();
+      }
+
+      // Adding to new items array location
+      prev[destination.droppableId].actions.splice(
+        destination.index,
+        0,
+        itemCopy
+      );
+
+      return prev;
+    });
   };
 
   return (
     <div className={classes.container}>
-      <React.Fragment>
-        <div className={classes.taskform}>
-          <h1 className={classes.taskform__heading_primary}> Add new task </h1>
-          <form onSubmit={addTask}>
-            <input
-              className={classes.taskform__input}
-              autoComplete="off"
-              name="content"
-              placeholder="Task Title"
-              value={task.content}
-              onChange={handleChange}
-            />
-            <button className={classes.taskform__button}>
-              <svg className={classes.taskform__icon}>
-                <use xlinkHref="#icon-plus"></use>
-              </svg>
-            </button>
-          </form>
-        </div>
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="TASKS" isDropDisabled={true}>
-            {(provided, snapshot) => (
-              <div
-                className={classes.taskbar}
-                ref={provided.innerRef}
-                isdraggingover={snapshot.isdraggingover}
-              >
-                {tasks &&
-                  tasks.map((currentTask, index) => {
-                    return (
-                      <Draggable
-                        key={currentTask.id}
-                        draggableId={currentTask.id}
-                        index={index}
-                      >
-                        {(provided, snapshot) => (
-                          <React.Fragment>
-                            <Task
-                              key={index}
-                              title={currentTask.content}
-                              provided={provided}
-                              snapshot={snapshot}
-                            />
-                            {snapshot.isDragging && (
-                              <div className={classes.clone}>
-                                {currentTask.content}
-                              </div>
-                            )}
-                          </React.Fragment>
-                        )}
-                      </Draggable>
-                    );
-                  })}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </React.Fragment>
+      <div className={classes.taskform}>
+        <h1 className={classes.taskform__heading_primary}> Add new task </h1>
+        <form onSubmit={addTask}>
+          <input
+            className={classes.taskform__input}
+            autoComplete="off"
+            name="content"
+            placeholder="Task Title"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+          />
+          <button className={classes.taskform__button}>
+            <svg className={classes.taskform__icon}>
+              <use xlinkHref="#icon-plus"></use>
+            </svg>
+          </button>
+        </form>
+      </div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        {_.map(tasks, (data, key) => {
+          return (
+            <Droppable
+              droppableId={data.title}
+              key={key}
+              isDropDisabled={data.title === "todo" ? true : false}
+            >
+              {(provided, snapshot) => {
+                return (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    key={data.title}
+                    className={classes.taskbar}
+                  >
+                    {data.actions.map((currentAction, index) => {
+                      return (
+                        <Draggable
+                          key={currentAction.id}
+                          index={index}
+                          draggableId={currentAction.id}
+                        >
+                          {(provided, snapshot) => {
+                            return (
+                              <React.Fragment>
+                                <Task
+                                  title={currentAction.name}
+                                  provided={provided}
+                                  snapshot={snapshot}
+                                />
+                                {snapshot.isDragging &&
+                                  data.title === "todo" && (
+                                    <div className={classes.clone}>
+                                      {currentAction.name}
+                                    </div>
+                                  )}
+                              </React.Fragment>
+                            );
+                          }}
+                        </Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </div>
+                );
+              }}
+            </Droppable>
+          );
+        })}
+      </DragDropContext>
     </div>
   );
 }
